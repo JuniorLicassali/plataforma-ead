cfa52b8ad8138007582e551184f2c68696408198
JuniorLicassali <licassali88@gmail.com>
2025-03-06 17:57:09 -0300

    first commit

---
+++ src/main/java/com/plataforma/plataforma_ead/PlataformaEadApplication.java
@@ -0,0 +1,13 @@
+package com.plataforma.plataforma_ead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaEadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaEadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plataforma/plataforma_ead/PlataformaEadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataforma.plataforma_ead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaEadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
